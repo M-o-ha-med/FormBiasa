18ddd85e9459304c315e37969e94c3e0163c8854
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2024-11-07 10:53:06 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -101,7 +101,31 @@
       $_SESSION['Nomor_telepon'] = $_POST['Nomor_telepon'];
       $_SESSION['Email'] =   $_POST['Email'];
       echo "<h3>Data berhasil disimpan!</h3>";
-      echo "<a href='/download'>Download csv</a>";
+      echo "<a href='/?download'>Download csv</a>";
+      }
+
+      if ($_SERVER['QUERY_STRING'] == download){
+        
+            session_start();
+            $nim = $_SESSION['NIM'];
+            $nama = $_SESSION['Nama'];
+            $gender = $_SESSION['Jenis_kelamin'];
+            $alamat = $_SESSION['Alamat'];
+            $notelp = $_SESSION['Nomor_telepon'];
+            $email = $_SESSION['Email'];
+            $data = array(["NIM","Nama","JenisKelamin","Alamat","No.telp","Email"],["$nim,$nama,$gender,$alamat,$notelp,$email"]);
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="data.csv"');
+            
+            $fp = fopen('php://output', 'w');
+            foreach ( $data as $line ) {
+            
+                fputcsv($fp, $line);
+            }
+            fclose($fp);
+
+
+
       }
       ?>
 
